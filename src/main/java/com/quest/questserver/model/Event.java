20395d7746fbd823e36a6eb525c09fffdbfba3c2
SiyuanChen0218 <siyuanchen@cmail.carleton.ca>
2022-02-24 17:44:24 -0500

    Update Story Cards
    Update Story Cards

--- src/main/java/com/quest/questserver/model/Event.java
+++ src/main/java/com/quest/questserver/model/Event.java
@@ -1,18 +1,44 @@
 package com.quest.questserver.model;
 
 public class Event extends Story{
-    @Override
-    public Quest getQuest(String quest) {
-        return null;
-    }
 
-    @Override
-    public Tournament getTournament(String tournament) {
-        return null;
-    }
+    String name;
+    String content;
 
-    @Override
-    public Event getEvent(String Event) {
+
+    public Event getEvent(String event) {
+        if (event.equalsIgnoreCase("Event_01")){
+            name = "Chivalrous Deed";
+            content = "Player(s) with both lowest rank and least amount of shields, receives 3 shields.";
+        }
+        if (event.equalsIgnoreCase("Event_02")){
+            name = "Court Called to Camelot";
+            content = "All Allies in play must be discarded.";
+        }
+        if (event.equalsIgnoreCase("Event_03")){
+            name = "King's Call to Arms";
+            content = "The highest ranked player(s) must place 1 weapon in the discard pile. If unable to do so, 2 Foe Cards must be discard.";
+        }
+        if (event.equalsIgnoreCase("Event_04")){
+            name = "King's Recognition";
+            content = "The next player(s) to complete a Quest will receive 2 extra shields.";
+        }
+        if (event.equalsIgnoreCase("Event_05")){
+            name = "Plague";
+            content = "Drawer loses 2 shields if possible.";
+        }
+        if (event.equalsIgnoreCase("Event_06")){
+            name = "Pox";
+            content = "All players except the player drawing this card lose 1 Shield.";
+        }
+        if (event.equalsIgnoreCase("Event_07")){
+            name = "Prosperity Throughout the Realm";
+            content = "All players may immediately draw 2 Adventure Cards.";
+        }
+        if (event.equalsIgnoreCase("Event_08")){
+            name = "Queen's Favor";
+            content = "The lowest ranked player(s) immediately receives 2 Adventure Cards.";
+        }
         return null;
     }
 }
--- src/main/java/com/quest/questserver/model/Tournament.java
+++ src/main/java/com/quest/questserver/model/Tournament.java
@@ -1,18 +1,56 @@
 package com.quest.questserver.model;
 
 public class Tournament extends Story{
-    @Override
-    public Quest getQuest(String quest) {
-        return null;
+
+    String name;
+    String shields;
+    int numShields;
+
+    public Tournament(String name, String shield, int numShields){
+        this.name = name;
+        shields = shield;
+        this.numShields = numShields;
     }
 
-    @Override
+    Tournament Tournament_01 = new Tournament("At Camelot", "3 bonus Shields", 3);
+
+
+    Tournament Tournament_02 = new Tournament("At Orkney", "2 bonus Shields", 2);
+
+
+    Tournament Tournament_03 = new Tournament("At Tintagel", "1 bonus Shields", 1);
+
+
+    Tournament Tournament_04 = new Tournament("At York", "0 bonus Shields", 0);
+
+    //Getter
+    public String getTournamentName() {
+        return name;
+    }
+
+    public String getTournamentShields() {
+        return shields;
+    }
+
+    public int getTournamentNumShields() {
+        return numShields;
+    }
+
     public Tournament getTournament(String tournament) {
+        if(tournament.equalsIgnoreCase("Tournament_01")){
+            return Tournament_01;
+        }
+        if(tournament.equalsIgnoreCase("Tournament_02")){
+            return Tournament_02;
+        }
+        if(tournament.equalsIgnoreCase("Tournament_03")){
+            return Tournament_03;
+        }
+        if(tournament.equalsIgnoreCase("Tournament_04")){
+            return Tournament_04;
+        }
         return null;
     }
 
-    @Override
-    public Event getEvent(String Event) {
-        return null;
-    }
+
 }

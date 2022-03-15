44ab8d1df4899fa164a9edd2c55e7d1b9f2fd28f
Linhong G <98068422+DophineYe@users.noreply.github.com>
2022-03-15 15:07:28 -0400

    Update EventStrategy.java

--- src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
+++ src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
@@ -12,4 +12,37 @@ public class EventStrategy {
     public void start(Game game){
         //check the name of the event then run the corresponding function of that event
     }
+
+    public void Event_01(Game game){
+
+    }
+
+    public void Event_02(Game game){
+
+    }
+
+    public void Event_03(Game game){
+
+    }
+
+    public void Event_04(Game game){
+
+    }
+
+    public void Event_05(Game game){
+
+    }
+
+    public void Event_06(Game game){
+
+    }
+
+    public void Event_07(Game game){
+
+    }
+
+    public void Event_08(Game game){
+
+    }
+
 }

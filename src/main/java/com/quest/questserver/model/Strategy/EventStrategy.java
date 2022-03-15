ee7406d4cd8802f28d3503318fe216a2b3edba6e
Linhong G <98068422+DophineYe@users.noreply.github.com>
2022-03-15 15:15:23 -0400

    Update EventStrategy.java

--- src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
+++ src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
@@ -11,6 +11,41 @@ public class EventStrategy {
 
     public void start(Game game){
         //check the name of the event then run the corresponding function of that event
+        switch (event.getTypeId()){
+            case "Event_01" :
+                Event_01(game);
+                break;
+
+            case "Event_02" :
+                Event_02(game);
+                break;
+
+            case "Event_03" :
+                Event_03(game);
+                break;
+
+            case "Event_04" :
+                Event_04(game);
+                break;
+
+            case "Event_05" :
+                Event_05(game);
+                break;
+
+            case "Event_06" :
+                Event_06(game);
+                break;
+
+            case "Event_07" :
+                Event_07(game);
+                break;
+
+            case "Event_08" :
+                Event_08(game);
+                break;
+        }
+
+
     }
 
     public void Event_01(Game game){

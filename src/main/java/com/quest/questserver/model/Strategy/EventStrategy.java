460a6575e1fb38c0c20fb5861cac8a3e238d23eb
Linhong G <98068422+DophineYe@users.noreply.github.com>
2022-03-15 14:48:08 -0400

    Update EventStrategy.java

--- src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
+++ src/main/java/com/quest/questserver/model/Strategy/EventStrategy.java
@@ -1,4 +1,25 @@
 package com.quest.questserver.model.Strategy;
 
-public class EventStrategy {
+import com.quest.questserver.model.Game;
+
+public class EventStrategy implements RoundStrategy{
+    @Override
+    public void start(Game game) {
+        
+    }
+
+    @Override
+    public void nextTurn(Game game) {
+
+    }
+
+    @Override
+    public void terminate(Game game) {
+
+    }
+
+    @Override
+    public int getRoundStatus() {
+        return 0;
+    }
 }
